7f1aa7c875f031d569dc683610650d8632ddfdc1
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2019-11-01 17:25:08 -0400

    feat: initial commit

---
+++ src/wait.ts
@@ -0,0 +1,9 @@
+export function wait(milliseconds: number) {
+  return new Promise(resolve => {
+    if (isNaN(milliseconds)) {
+      throw new Error("milliseconds not a number");
+    }
+
+    setTimeout(() => resolve("done!"), milliseconds);
+  });
+}
