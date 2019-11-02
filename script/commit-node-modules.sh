be82549b9e3fbab0b3bd3de48cfd18d7a935b7e0
Richard Harrah <topplethenunnery@gmail.com>
2019-11-02 10:14:24 -0400

    ci(release): fix dist folder not being checked in

--- script/commit-node-modules.sh
+++ script/commit-node-modules.sh
@@ -2,6 +2,7 @@
 
 rm -rf node_modules
 sed -i '/node_modules/d' .gitignore # Bash command that removes node_modules from .gitignore
+sed -i '/dist/d' .gitignore # Bash command that removes dist from .gitignore
 yarn install --frozen-lockfile --production
-git add node_modules .gitignore
-git commit -m "chore(release): add node_modules"
+git add dist node_modules .gitignore
+git commit -m "chore(release): add node_modules and dist"
