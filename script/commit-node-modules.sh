f61945feb06440cc033c1e5f01af8bbb281c20dc
Richard Harrah <topplethenunnery@gmail.com>
2019-11-02 10:20:50 -0400

    ci(release): use action-semantic-release

--- script/commit-node-modules.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-rm -rf node_modules
-sed -i '/node_modules/d' .gitignore # Bash command that removes node_modules from .gitignore
-sed -i '/dist/d' .gitignore # Bash command that removes dist from .gitignore
-yarn install --frozen-lockfile --production
-git add dist node_modules .gitignore
-git commit -m "chore(release): add node_modules and dist"
